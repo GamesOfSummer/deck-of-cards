25c43cf515e565e06d6fe25b2f80a12652722185
Summer <summer.d.howard@gmail.com>
2019-04-23 08:56:06 -0500

    completed - (this commit was rebased, FYI)

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var casino = new Casino();
+        var deck = casino.CreateDeck();
+        deck = casino.ShuffleDeck(deck);
+        casino.DisplayDeck(deck);
+
+    }
+}
+
+
+public class Card
+{
+    public string value { get; set; }
+}
+
+public class Casino
+{
+    public List<Card> CreateDeck()
+    {
+        List<Card> deck = new List<Card>();
+        List<string> royalCardFaces = new List<string> { "Jack", "Queen", "King" };
+        List<string> suits = new List<string> { "Club", "Heart", "Spade", "Diamond" };
+
+
+        foreach (string suit in suits)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                deck.Add(new Card() { value = suit + " - " + i });
+            }
+
+            foreach (string face in royalCardFaces)
+            {
+                deck.Add(new Card() { value = suit + " - " + face });
+            }
+        }
+
+        return deck;
+
+    }
+
+    public List<Card> ShuffleDeck(List<Card> deck)
+    {
+        return deck.OrderBy(a => Guid.NewGuid()).ToList();
+    }
+
+    public void DisplayDeck(List<Card> deck)
+    {
+        foreach (Card card in deck)
+        {
+            Console.WriteLine(">> " + card.value);
+        }
+    }
+
+}
+
+
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # deck-of-cards
-Code Kata - create a deck of cards, then shuffle them
+
+April 25th
+
+**Code Kata** - create a deck of cards, then shuffle them
+
+Output is
+
+> > Diamond - 8  
+> > Spade - King  
+> > Heart - 3  
+> > Diamond - 5  
+> > Club - 6  
+> > Spade - 10  
+> > ...
+
+While this can be solved in two lines, this was the solution that I came up with on the spot. (I did have to look up the LINQ shuffle, obviously)
+
+I'm pretty proud of myself for populating a deck instead of writing out every card by hand.
